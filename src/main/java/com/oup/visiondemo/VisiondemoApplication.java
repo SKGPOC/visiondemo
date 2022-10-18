d3cdc85ac97001125156dbd7220c06b849aee3e2
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-10-18 20:40:24 +0100

    Initial commit

---
+++ src/main/java/com/oup/visiondemo/VisiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.oup.visiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisiondemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/oup/visiondemo/route/CamelRoute.java
@@ -0,0 +1,21 @@
+package com.oup.visiondemo.route;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CamelRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        from("timer://foo?fixedRate=true&period=60000")
+                .log(LoggingLevel.INFO,log,"Hello World!!!!")
+                .to("http4://api.coronavirus.data.gov.uk/generic/metrics?category=Cases")
+                .log(LoggingLevel.INFO,log,"${body}");
+
+
+    }
+}
