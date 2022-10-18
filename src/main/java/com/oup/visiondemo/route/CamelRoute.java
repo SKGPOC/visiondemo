8889ca8bf6a32589692cbc062a8d378cdb8830c8
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-10-18 23:06:03 +0100

    NA

--- src/main/java/com/oup/visiondemo/route/CamelRoute.java
+++ src/main/java/com/oup/visiondemo/route/CamelRoute.java
@@ -12,9 +12,14 @@ public class CamelRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         from("timer://foo?fixedRate=true&period=10000")
-                .log(LoggingLevel.INFO,log,"Hello World!!!!")
-                .to("http4://api.coronavirus.data.gov.uk/generic/metrics?category=Cases&proxyAuthHost=ouparray.oup.com&proxyAuthPort=8080")
-                .log(LoggingLevel.INFO,log,"${body}");
+                .log(LoggingLevel.INFO, log, "Hello World!!!!")
+                .to("https://api.coronavirus.data.gov.uk/generic/area/region?proxyAuthHost=ouparray.oup.com&proxyAuthPort=8080")
+                .split().jsonpath("$").streaming()
+                .split().jsonpath("$.areaCode")
+                .setHeader("URI", simple("https://api.coronavirus.data.gov.uk/generic/soa/region/${body}/newCasesBySpecimenDate?httpMethod=GET&proxyAuthHost=ouparray.oup.com&proxyAuthPort=8080&throwExceptionOnFailure=true"))
+                .log(LoggingLevel.INFO, log, "${headers.URI}")
+                .toD("${headers.URI}")
+                .log(LoggingLevel.INFO, log, "${body}");
 
 
     }
